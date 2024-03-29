d91f9e6ebb338338ec15bb4efce03e0c84f3a675
김선규 <seongyu1066@naver.com>
2024-03-29 16:01:08 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,28 +1,25 @@
 function solution(n) {
     var answer = 0;
     
-    //1. n = result * 6
-    //2. n = (result * 6) / slice
+    // if(n % 6 == 0) answer = n / 6;
+    // else if(n % 6 != 0) {
+    //     for(let i = 2; i < 100; i++) {
+    //         if((n * i) % 6 != 0) {
+    //             continue;
+    //         } else if ((n * i) % 6 === 0) {
+    //             answer = (n * i) / 6;
+    //             break;
+    //         }
+    //     }
+    // }
     
-    //slice = (result * 6) / n
-    
-    // n = (result * 6) / ((result * 6) / n)
-    // n * ((result * 6) / n) = (result * 6)
-    // (n * ((result * 6) / n)) / 6 = result 
-    
-    // answer = (n * ((answer * 6) / n)) / 6
-    
-    if(n % 6 == 0) answer = n / 6;
-    
-    else if(n % 6 != 0) {
-        for(let i = 2; i < 100; i++) {
-            if((n * i) % 6 != 0) {
-                continue;
-            } else if ((n * i) % 6 === 0) {
-                answer = (n * i) / 6;
-                break;
-            }
+    var i = 1;
+    while(true) {
+        if((n * i) % 6 == 0){
+            answer = (n * i) / 6;
+            break;
         }
+        i++;
     }
     return answer;
 }
