651e8a1917d12848b428eff48d3d585fba0b2ac2
김선규 <seongyu1066@naver.com>
2024-03-31 16:01:46 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,8 +1,3 @@
 function solution(arr) {
-    var answer = 0;
-    
-    for(let i = 0; i < arr.length; i++) {
-        answer += arr[i];
-    }
-    return answer/(arr.length);
+    return arr.reduce((a, b) => a + b ) / arr.length;
 }
