5e5c591062c48c5f9aa2f229feb990f254219274
김선규 <seongyu1066@naver.com>
2024-04-03 15:50:50 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,16 +1,17 @@
-function solution(phone_number) {
-    var answer = '';
+function solution(s) {
+//     var answer = '';
     
-    phone_number = phone_number.split("");
+//     phone_number = phone_number.split("");
     
-    for(let i = 0; i < phone_number.length - 4; i++){
-        phone_number.splice(i, 1, "*");
-    }
+//     for(let i = 0; i < phone_number.length - 4; i++){
+//         phone_number.splice(i, 1, "*");
+//     }
     
-    console.log(phone_number);
-    for(let el of phone_number) {
-        answer += el;
-    }
+//     console.log(phone_number);
+//     for(let el of phone_number) {
+//         answer += el;
+//     }
     
-    return answer;
+//     return answer;
+    return s.replace(/\d(?=\d{4})/g, "*");
 }
