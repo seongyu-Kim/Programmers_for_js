daf2e16f36a9f87a118c59185556d7bfe90352f3
김선규 <seongyu1066@naver.com>
2024-04-05 15:16:52 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,11 @@
+function solution(my_string, num1, num2) {
+    var answer = [...my_string];
+    
+    
+    
+    answer.splice(num1, 1, my_string[num2]);
+    answer.splice(num2, 1, my_string[num1]);
+    
+    
+    return answer.join('');
+}
