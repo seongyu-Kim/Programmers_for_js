769d66e2d9c93fc897889f1561ff5ec981f1a726
김선규 <seongyu1066@naver.com>
2024-04-16 15:54:31 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    
+    function gcd(x, y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+    
+    const g = gcd(a, b);
+    a /= g;
+    b /= g;
+    
+    while (b % 2 === 0) b /= 2;
+    while (b % 5 === 0) b /= 5;
+
+    return b === 1 ? 1 : 2;
+        
+}
