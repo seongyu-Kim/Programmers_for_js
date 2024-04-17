42f5d1fd602eb7efb3e805d78fbc2372947c8f13
김선규 <seongyu1066@naver.com>
2024-04-17 17:55:10 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -3,16 +3,14 @@ function solution(id_pw, db) {
     var id = id_pw[0];
     var pw = id_pw[1];
     
-    db = db.flat();
-    
-    for(let i = 0; i < db.length; i++) {
-        if(id === db[i]) {
-            if(pw === db[i + 1]) {
-                return "login";
-            } else if (pw != db[i + 1]) {
-                return "wrong pw";
-            }
-        } 
+    for(let el of db) {
+        let dbId = el[0];
+        let dbPw = el[1];
+        
+        if(id === dbId) {
+            if(pw === dbPw) return "login";
+            else return "wrong pw";
+        }
     }
     
     
