1aef37beb84a5ef7696f2c3d5d63079b6d6b8987
김선규 <seongyu1066@naver.com>
2024-05-01 14:37:55 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -12,7 +12,7 @@ function solution(nums) {
     function isPrime(num) {
         if(num === 2) return true;
         
-        for(let i = 2; i < num; i++) {
+        for(let i = 2; i <= Math.sqrt(num); i++) {
             if(num % i === 0) {
                 return false;
             }
