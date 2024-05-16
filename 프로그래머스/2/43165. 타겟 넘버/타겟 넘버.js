3aa86f5aacefbe7e3ab072a7c78ab23515fb1c21
김선규 <seongyu1066@naver.com>
2024-05-16 16:11:40 +0900

    [level 2] Title: 타겟 넘버, Time: 1.17 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,18 @@
+function solution(numbers, target) {
+    var answer = 0;
+    
+    const dfs = (sum, idx) => {
+        
+        if(idx === numbers.length) {
+            if(sum === target) answer++;
+            return;
+        }
+        
+        dfs(sum + numbers[idx], idx + 1);
+        dfs(sum - numbers[idx], idx + 1);
+    };
+    
+        dfs(0, 0);
+    
+    return answer;
+}
