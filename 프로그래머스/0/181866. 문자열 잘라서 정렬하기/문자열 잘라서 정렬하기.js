7639b111b787ca32117337655115396422d2d6ec
김선규 <seongyu1066@naver.com>
2024-05-25 23:54:07 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 9.00 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -0,0 +1,13 @@
+function solution(myString) {
+    var answer = [];
+    
+    let tmps = myString.split('x');
+    
+    tmps = tmps.filter(tmp => tmp !== "");
+    
+    
+    
+    
+    
+    return tmps.sort();
+}
